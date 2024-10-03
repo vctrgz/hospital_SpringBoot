413f8d19e969e4fc642f7d5b9934ddaa7aac896d
VictorGonzalez <goncavic2002@gmail.com>
2024-10-03 16:54:46 +0200

    Merge branch 'master' of https://github.com/vctrgz/hospital_SpringBoot.git

--- src/main/java/SpringBoot/Hospital/NurseController.java
+++ src/main/java/SpringBoot/Hospital/NurseController.java
@@ -1,5 +1,18 @@
 package SpringBoot.Hospital;
 
-public class NurseController {
+import java.util.ArrayList;
 
+public class NurseController {
+	ArrayList <Nurse> nurses;
+	public NurseController() {
+		nurses = new ArrayList<Nurse>();
+		nurses.add(new Nurse("Juan", "juan@nurse.com", "123"));
+		nurses.add(new Nurse("Jose", "jose@nurse.com", "123"));
+		nurses.add(new Nurse("Carlos", "carlos@nurse.com", "123"));
+		nurses.add(new Nurse("Javi", "javi@nurse.com", "123"));
+	}
+	public boolean login(String user, String password) {
+		
+		return false;
+	}
 }
