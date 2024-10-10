284713240bc7443611bfbbb3d0d636f392ac64d8
VictorGonzalez <goncavic2002@gmail.com>
2024-10-10 16:35:32 +0200

    New class LoginRequest. Issue 2 Login method.

--- src/main/java/SpringBoot/Hospital/LoginRequest.java
+++ src/main/java/SpringBoot/Hospital/LoginRequest.java
@@ -1,24 +1,9 @@
 package SpringBoot.Hospital;
 
-public class Nurse {
-
-	String name;
+public class LoginRequest {
 	String user;
 	String password;
  
-	public Nurse(String name, String user, String password){
-		 this.name = name;
-		 this.password = password;
-		 this.user = user;
-	}
- 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 	public String getUser() {
 		return user;
 	}
--- src/main/java/SpringBoot/Hospital/NurseController.java
+++ src/main/java/SpringBoot/Hospital/NurseController.java
@@ -1,7 +1,17 @@
 package SpringBoot.Hospital;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nurse")
 public class NurseController {
 	ArrayList <Nurse> nurses;
 	public NurseController() {
@@ -11,8 +21,24 @@ public class NurseController {
 		nurses.add(new Nurse("Carlos", "carlos@nurse.com", "123"));
 		nurses.add(new Nurse("Javi", "javi@nurse.com", "123"));
 	}
-	public boolean login(String user, String password) {
-		
-		return false;
+	
+	@PostMapping("/login")	
+	public Map<String, Object> login(@RequestBody LoginRequest loginRequest) {
+		Map<String, Object> response = new HashMap<>();
+		boolean loginCorrecto = false;
+		for (Nurse nurse : nurses) {
+			if (nurse.getUser().equals(loginRequest.getUser()) && nurse.getPassword().equals(loginRequest.getPassword())) {
+				loginCorrecto = true;
+				break;
+			}
+		}
+		if (loginCorrecto) {
+			response.put("status", "succes");
+			response.put("message", "Login successful");
+		}else {
+			response.put("status", "error");
+			response.put("message", "Invalid username or password");
+		}
+		return response;
 	}
 }
