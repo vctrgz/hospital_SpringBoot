6b569716de0f271e0c6ece865e28d14bae2ddb5c
TheSky189 <kenkaneki94tg@gmail.com>
2024-10-11 16:49:26 +0200

    resolve conflict

--- src/main/java/SpringBoot/Hospital/Nurse.java
+++ src/main/java/SpringBoot/Hospital/Nurse.java
@@ -1,39 +1,38 @@
 package SpringBoot.Hospital;
 
 public class Nurse {
-	String name;
-	String user;
-	String password;
- 
-public Nurse(String name, String user, String password){
-	this.name = name;
-	this.password = password;
-	this.user = user;
- }
- 
- public String getName() {
-	 return name;
- }
- 
- public void setName(String name) {
-	 this.name = name;
- }
- 
- public String getUser() {
-	 return user;
- }
- 
- public void setUser(String user) {
-	 this.user = user;
- }
- 
- public String getPassword() {
-	 return password;
- }
- 
- public void setPassword(String password) {
-	 this.password = password;
- }
- 
+
+    String name;
+    String user;
+    String password;
+
+    public Nurse(String name, String user, String password){
+        this.name = name;
+        this.password = password;
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
- 
--- src/main/java/SpringBoot/Hospital/NurseController.java
+++ src/main/java/SpringBoot/Hospital/NurseController.java
@@ -1,10 +1,14 @@
 package SpringBoot.Hospital;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
