4e92f4e0da1d2fd5ca0ea96ccf39f367a4dc52f4
TheSky189 <kenkaneki94tg@gmail.com>
2024-10-11 16:32:08 +0200

    order

--- src/main/java/SpringBoot/Hospital/Nurse.java
+++ src/main/java/SpringBoot/Hospital/Nurse.java
@@ -1,15 +1,14 @@
 package SpringBoot.Hospital;
 
 public class Nurse {
-
-String name;
-String user;
-String password;
+	String name;
+	String user;
+	String password;
  
- public Nurse(String name, String user, String password){
-	 this.name = name;
-	 this.password = password;
-	 this.user = user;
+public Nurse(String name, String user, String password){
+	this.name = name;
+	this.password = password;
+	this.user = user;
  }
  
  public String getName() {
