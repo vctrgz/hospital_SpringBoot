9c853a8e514c9dc053ae87f5934b7c15a6d37aa1
VictorGonzalez <goncavic2002@gmail.com>
2024-10-11 21:53:05 +0200

    ResponseEntity issue

--- src/main/java/SpringBoot/Hospital/NurseController.java
+++ src/main/java/SpringBoot/Hospital/NurseController.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,7 +26,7 @@ public class NurseController {
 	}
 	
 	@PostMapping("/login")	
-	public Map<String, Object> login(@RequestBody LoginRequest loginRequest) {
+	public @ResponseBody ResponseEntity<Boolean> login(@RequestBody LoginRequest loginRequest) {
 		Map<String, Object> response = new HashMap<>();
 		boolean loginCorrecto = false;
 		for (Nurse nurse : nurses) {
@@ -33,12 +36,9 @@ public class NurseController {
 			}
 		}
 		if (loginCorrecto) {
-			response.put("status", "succes");
-			response.put("message", "Login successful");
+			return ResponseEntity.ok(loginCorrecto);
 		}else {
-			response.put("status", "error");
-			response.put("message", "Invalid username or password");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(loginCorrecto);
 		}
-		return response;
 	}
 }
