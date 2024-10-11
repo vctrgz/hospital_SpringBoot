7fee2f73678a9a90bc924351a9fbcaf2ee59a88f
albertsalom <albertsaloom@gmail.com>
2024-10-11 18:05:03 +0200

    Update Nurse

--- src/main/java/SpringBoot/Hospital/Nurse.java
+++ src/main/java/SpringBoot/Hospital/Nurse.java
@@ -14,28 +14,4 @@ public class Nurse {
 	 this.user = user;
  }
  
- public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
- 
 }
