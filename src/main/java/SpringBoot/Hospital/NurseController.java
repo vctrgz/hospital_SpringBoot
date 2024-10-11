8c162962e123e240805c50fadb1cb04e6b42c7f0
albertsalom <albertsaloom@gmail.com>
2024-10-11 18:01:41 +0200

    Information of all registered nurses

--- src/main/java/SpringBoot/Hospital/NurseController.java
+++ src/main/java/SpringBoot/Hospital/NurseController.java
@@ -1,9 +1,14 @@
 package SpringBoot.Hospital;
-
 import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/nurse")
 
 public class NurseController {
-	ArrayList <Nurse> nurses;
+	
+	private List<Nurse> nurses;
+	
 	public NurseController() {
 		nurses = new ArrayList<Nurse>();
 		nurses.add(new Nurse("Juan", "juan@nurse.com", "123"));
@@ -11,8 +16,15 @@ public class NurseController {
 		nurses.add(new Nurse("Carlos", "carlos@nurse.com", "123"));
 		nurses.add(new Nurse("Javi", "javi@nurse.com", "123"));
 	}
+	
+	@GetMapping("/nurses")
+	public List<Nurse> getAllNurses(){
+		return nurses;
+	}
+	
 	public boolean login(String user, String password) {
-		
 		return false;
 	}
+	
+	
 }
