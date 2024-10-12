5e3877f5f42eae6bff0b634a00e95fd768dfb859
VictorGonzalez <goncavic2002@gmail.com>
2024-10-12 14:17:00 +0200

    ResponseEntity Issue 3

--- src/main/java/SpringBoot/Hospital/NurseController.java
+++ src/main/java/SpringBoot/Hospital/NurseController.java
@@ -1,13 +1,12 @@
 package SpringBoot.Hospital;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/nurse")
 public class NurseController {
-	ArrayList <Nurse> nurses;
-	public NurseController() {
-		nurses = new ArrayList<Nurse>();
-		nurses.add(new Nurse("Juan", "juan@nurse.com", "123"));
-		nurses.add(new Nurse("Jose", "jose@nurse.com", "123"));
-		nurses.add(new Nurse("Carlos", "carlos@nurse.com", "123"));
-		nurses.add(new Nurse("Javi", "javi@nurse.com", "123"));
-	}
-	@GetMapping("/nurses")
+    
+    private List<Nurse> nurses;
+    
+    public NurseController() {
+        nurses = new ArrayList<>();
+        nurses.add(new Nurse("Juan", "juan@nurse.com", "123"));
+        nurses.add(new Nurse("Jose", "jose@nurse.com", "123"));
+        nurses.add(new Nurse("Carlos", "carlos@nurse.com", "123"));
+        nurses.add(new Nurse("Javi", "javi@nurse.com", "123"));
+    }
+
+    // Get all registered nurses
+    @GetMapping("/nurses")
     public List<Nurse> getAllNurses() {
         return nurses;
     }
-	@PostMapping("/login")	
+    
+    // Login functionality
+    @PostMapping("/login")	
 	public @ResponseBody ResponseEntity<Boolean> login(@RequestBody LoginRequest loginRequest) {
-		Map<String, Object> response = new HashMap<>();
 		boolean loginCorrecto = false;
 		for (Nurse nurse : nurses) {
 			if (nurse.getUser().equals(loginRequest.getUser()) && nurse.getPassword().equals(loginRequest.getPassword())) {
@@ -45,4 +49,16 @@ public class NurseController {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(loginCorrecto);
 		}
 	}
+
+    // Find nurse by name
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Nurse> findByName(@PathVariable String name) {
+        for (Nurse nurse : nurses) {
+            if (name.equals(nurse.getName())) {
+                return ResponseEntity.ok(nurse);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
+	
