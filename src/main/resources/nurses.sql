0716edd10b8be58a4615a955e59850af876e0009
TheSky189 <kenkaneki94tg@gmail.com>
2024-10-25 01:56:35 +0200

    last update

---
+++ src/main/resources/nurses.sql
@@ -0,0 +1,4 @@
+INSERT INTO nurses (id, name, user, password) VALUES (1,'Juan', 'juan@nurse.com', '123');
+INSERT INTO nurses (id, name, user, password) VALUES (2, 'Jose', 'jose@nurse.com', '123');
+INSERT INTO nurses (id, name, user, password) VALUES (3, 'Carlos', 'carlos@nurse.com', '123');
+INSERT INTO nurses (id, name, user, password) VALUES (4, 'Javi', 'javi@nurse.com', '123');
